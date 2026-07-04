afa4c15c5fddc46a980ac11f4ed8e419fb3d6738
chvlswetha <chvl.swetha@gmail.com>
2026-07-04 01:06:05 -0700

    Added sendSuccess method utility

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,9 +1,11 @@
 import { Request, Response} from "express";
 import { findAllUsers as findAllUsersService, findById as findByIdService} from "../services/users.service.js";
+import { sendSuccess } from "../utils/api-response.js";
 
 export async function findAllUsers(_req : Request, res: Response) {  //Called from Router
     const response = await findAllUsersService();  //controller will call service
-    res.json(response); //gives the response back to router that is in app object
+    //res.json(response); //gives the response back to router that is in app object
+    sendSuccess(res, response); //this function gives more structured response
 }
 
 export async function findById(_req: Request, res: Response) {  
@@ -11,7 +13,8 @@ export async function findById(_req: Request, res: Response) {
     console.log("Inside Controller");
     const response = await findByIdService(Number(id));  // convert the string id to number to the service
 
-    res.json(response); //gives the response back to router that is in app object
+    //res.json(response); //gives the response back to router that is in app object
+    sendSuccess(res, response); //gives the response back to router that is in app object
 }
 
 export async function createUser(_req: Request, res: Response) {
