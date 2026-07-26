824163f2ff960fd75e84ce9ff2c6d0fe958a3220
chvlswetha <chvl.swetha@gmail.com>
2026-07-25 21:41:51 -0700

    Added custom Zod Validators for Availbilty rules and Availability Exceptions

--- src/dtos/availability.dto.ts
+++ src/dtos/availability.dto.ts
@@ -3,7 +3,7 @@ import { z } from "zod";
 const timeRegex = /^([01]\d|2[0-3]):[0-5]\d$/;
 const dateRegex = /^\d{4}-0[1-9]|1[0-2]-0[1-9]|[12]\d|3[01]$/
 
-export const createAvailabilityRuleSchema = z.object({
+export const createAvailabilityRuleBaseSchema = z.object({
     weekday: z.number().int().min(0).max(6),
     startTime: z.string().regex(timeRegex, "Start time must be in HH:MM format"),
     endTime: z.string().regex(timeRegex, "End time must be in HH:MM format"),
@@ -11,19 +11,40 @@ export const createAvailabilityRuleSchema = z.object({
     timezone: z.string().default("UTC")
 });
 
+//custom rule start time < end time. So extendinmg BaseSchema with Refine method
+export const createAvailabilityRuleSchema  = createAvailabilityRuleBaseSchema.refine(
+    (rule) => rule.startTime < rule.endTime,
+    {message: "Start time must be less than the End time"}
+);
+
 export const updateAvailabilityRuleSchema = createAvailabilityRuleSchema.partial();
 
 export type createAvailabilityRuleDto = z.infer<typeof createAvailabilityRuleSchema>;
 export type updateAvailabilityRuleDto = z.infer<typeof updateAvailabilityRuleSchema>;
 
+//custom rule for startTime and Endtime that should be available for non=-full day block.
+// And  start time < end time. So extendinmg BaseSchema with superRefine method
+//SuperEfine Methos adds the error mesages to the path specified(startime,endTime etc..)
+
 export const createAvailabilityExceptionSchema = z.object({
-    date: z.string().regex(dateRegex,"rDate must be in MMMM-YY_DD format"),
+    date: z.string().regex(dateRegex,"Date must be in MMMM-YY_DD format"),
     type: z.enum(["BLOCK_FULL_DAY", "BLOCK_PARTIAL","Add_AVAILABILITY WINDOW"]),
     startTime: z.string().regex(timeRegex, "Start time must be in HH:MM format"),
     endTime: z.string().regex(timeRegex, "End time must be in HH:MM format"),
     timezone: z.string().default("UTC"),
     reason: z.string().min(0).max(500).optional()
+}).superRefine((data,ctx) => {
+            if(data.type !== "BLOCK_FULL_DAY")
+             {
+                 if(!data.startTime)
+                    ctx.addIssue({path: ['startTime'], code : "custom", message: "StartTime is required for Non-Full day Exception"})
+                 if(!data.endTime)
+                    ctx.addIssue({path: ['endTime'], code : "custom", message: "endime is required for Non-Full day Exception"})
+                if(data.startTime && data.endTime && data.startTime >= data.endTime)
+                    ctx.addIssue({path: ['endTime'], code : "custom", message: "StartTime should be before the End time"})
+             }
 });
+                 
 
 export const updateAvailabilityExceptionSchema = createAvailabilityExceptionSchema.partial();
 
